fee54b5a56fbf23bbe401d512c970de76ae60cf3
daylon <daylonvanwel@gmail.com>
2015-02-09 16:40:17 +0100

    Initial Commit

---
+++ holdGames/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  holdGames
+//
+//  Created by daylonvanwel on 09-02-15.
+//  Copyright (c) 2015 daylon wel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
